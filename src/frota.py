1677ba66498ed364f8e601974ade13324a7fe902
ferauche <thiago.ferauche@gmail.com>
2024-08-18 23:34:04 -0300

    primeira versao aula 1 classe Carro

---
+++ src/frota.py
@@ -0,0 +1,43 @@
+class Carro:
+    modelo : str
+    marca : str
+    cor : str
+    odometro : 0.0
+    motor_on : False
+
+    def __init__(self, modelo : str, marca : str, cor : str,
+                       odometro : float, motor : bool):
+        self.modelo = modelo
+        self.marca = marca
+        self.cor = cor
+        self.odometro = odometro
+        self.motor_on = motor
+
+    def ligar(self):
+        if not self.motor_on:
+            self.motor_on = True
+        else:
+            raise Exception("Erro: Motor já ligado!")
+
+    def acelerar(self, velocidade : float, tempo : float):
+        if self.motor_on:
+            self.odometro += velocidade * tempo
+        else:
+            raise Exception("Erro: Não é possível acelerar! Motor desligado!")
+
+    def desligar(self):
+        if self.motor_on:
+            self.motor_on = False
+        else:
+            raise Exception("Erro: Motor já desligado!")
+
+    def __str__(self):
+        info = (f'Carro {self.modelo}, marca {self.marca}, '
+                f'cor {self.cor}\n{self.odometro} Km, '
+                f'motor {self.motor_on}')
+        return info
+
+
+
+
+
