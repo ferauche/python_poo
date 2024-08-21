7eaf50f17b199976d7ac93f97a825244ef91f9f0
furryCEGO <furrycego@gmail.com>
2024-08-21 09:42:20 -0300

    Simulação com dois carros, feito em aula.

--- src/main.py
+++ src/main.py
@@ -1,44 +1,67 @@
 from frota import *
+def operar_carro(carro : Carro):
+    print('1- Ligar motor')
+    print('2- Desligar motor')
+    print('3- Acelerar')
+
+    op = 0
+    while op not in (1, 2, 3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+    if op == 1:
+        carro.ligar()
+    elif op == 2:
+        carro.desligar()
+    elif op == 3:
+        v = float(input("Informe a velocidade: "))
+        t = float(input("Informe o tempo: "))
+        carro.acelerar(v, t)
+
+    print('Infos atuais do carro')
+    print(carro)
 
 if __name__ == "__main__":
-    print('Cadastre um carro')
-    nm_modelo = input('Digite o modelo: ')
-    nm_marca = input('Digite a marca: ')
-    nm_cor = input('Digite a cor: ')
+    print('Cadastre o primeiro carro')
+    nm_modelo1 = input('Digite o modelo: ')
+    nm_marca1 = input('Digite a marca: ')
+    nm_cor1 = input('Digite a cor: ')
 
-    kms = float(input('Digite com quantos Kms: '))
+    carro1 = Carro(nm_modelo1, nm_marca1, nm_cor1, 0, motor=False)
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    print('Cadastre um segundo carro')
+    nm_modelo2 = input('Digite o modelo: ')
+    nm_marca2 = input('Digite a marca: ')
+    nm_cor2 = input('Digite a cor: ')
+
+    carro2 = Carro(nm_modelo2, nm_marca2, nm_cor2, 0, motor=False)
 
     '''
-    Controlando o carro até ele atingir 10000 Km
+    Controlando dois carro até atingir 600 Km
     '''
-    while carro1.odometro < 10000:
+    while carro1.odometro < 600 and carro2.odometro < 600:
         try:
-            print('1- Ligar motor')
-            print('2- Desligar motor')
-            print('3- Acelerar')
-
+            print('Escolha um carro:')
+            print(f'Carro 1: {nm_modelo1} {nm_cor1}')
+            print(f'Carro 2: {nm_modelo2} {nm_cor2}')
             op = 0
-            while op not in (1,2,3):
-                op = int(input("Digite as opcoes[1-3]: "))
-
+            while op not in (1, 2):
+                op = int(input("Digite as opcoes[1 ou 2]: "))
             if op == 1:
-                carro1.ligar()
-            elif op == 2:
-                carro1.desligar()
-            elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
-
-            print('Infos atuais do carro')
-            print(carro1)
+                operar_carro(carro1)
+            else:
+                operar_carro(carro2)
         except Exception as e:
             print("Erro!")
             print(e)
 
     carro1.desligar()
+    carro2.desligar()
     print(carro1)
-    print('Parar para trocar óleo!!!')
+    print(carro2)
+    if carro1.odometro > carro2.odometro:
+        print(f'{nm_modelo1} {nm_cor1} chegou primeiro que o {nm_modelo2} {nm_cor2}')
+    elif carro1.odometro == carro2.odometro:
+        print(f'{nm_modelo1} {nm_cor1} chegou ao mesmo tempo {nm_modelo2} {nm_cor2}')
+    else:
+        print(f'{nm_modelo2} {nm_cor2} chegou primeiro que o {nm_modelo1} {nm_cor1}')
 
