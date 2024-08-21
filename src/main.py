1593c51840d7a49fc2d4afd91d1468699a7a942c
Flavin324 <jo.santos@unisantos.br>
2024-08-21 10:51:41 -0300

    Simulando dois carros. alterando variaveis como tanque e combu

--- src/main.py
+++ src/main.py
@@ -24,19 +24,23 @@ if __name__ == "__main__":
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Litros do tanque: '))
+    consu = float(input('Consumo medio: '))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0 ,False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0 ,False, litros, consu)
 
     print('Cadastre o segundo carro')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Litros do tanque: '))
+    consu = float(input('Consumo medio: '))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0 ,False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0 ,False, litros, consu)
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and (carro1.tanque > 0 or carro2.tanque > 0 ):
         try:
             op_carro = 0
             while op_carro not in [1, 2]:
@@ -50,8 +54,4 @@ if __name__ == "__main__":
         except Exception as e:
             print("Erro!")
             print(e)
-
-    carro1.desligar()
-    print(carro1)
-    print('Parar para trocar óleo!!!')
 
