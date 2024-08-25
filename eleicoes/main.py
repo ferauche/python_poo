f4c38ca5a2b06bc8420393077e365a11c893884f
ferauche <thiago.ferauche@gmail.com>
2024-08-25 09:37:38 -0300

    Inicio projeto eleicao

---
+++ eleicoes/main.py
@@ -0,0 +1,83 @@
+import pickle
+import traceback
+from inspect import trace
+
+from common import *
+
+FILE_ELEITORES = 'eleitores.pkl'
+
+def menu_eleitor():
+    print("1-Novo Eleitor")
+    print("2-Atualizar Eleitor")
+    print("3-Sair")
+    op = int(input("Digite a opcao [1,2,3]? "))
+    while op not in (1, 2, 3):
+        op = int(input("Digite a opcao [1,2,3]? "))
+    return op
+
+def inserir_eleitor(eleitores):
+    titulo = int(input("Digite o Títlulo: "))
+
+    if titulo in eleitores:
+        raise Exception("Titulo já existente!")
+
+    nome = input("Digite o nome: ")
+    RG = input("Digite o RG: ")
+    CPF = input("Digite o CPF: ")
+    secao = input("Digite a secao: ")
+    zona = input("Digite a zona: ")
+
+    eleitor = Eleitor(nome, RG, CPF, titulo, secao, zona)
+    eleitores[eleitor.get_titulo()] = eleitor
+
+    with open(FILE_ELEITORES, 'wb') as arquivo:
+        pickle.dump(eleitores, arquivo)
+
+    print('Eleitor gravado com sucesso!')
+    print(eleitor)
+
+def atualizar_eleitor(eleitores):
+    titulo = int(input('Digite o titulo do eleitor: '))
+
+    if titulo in eleitores:
+        eleitor = eleitores[titulo]
+        print(eleitor)
+        secao = input("Digite a nova secao: ")
+        zona = input("Digite a nova zona: ")
+        eleitor.secao = secao
+        eleitor.zona = zona
+
+        with open(FILE_ELEITORES, 'wb') as arquivo:
+            pickle.dump(eleitores, arquivo)
+
+        print('Atualizados dados do eleitor!')
+        print(eleitor)
+    else:
+        raise Exception('Titulo inexistente')
+
+if __name__ == "__main__":
+    eleitores = {} #dicionário a chave será o titulo
+    try:
+        print("Carregando arquivo de eleitores ...")
+
+        with open('eleitores.pkl', 'rb') as arquivo:
+            eleitores = pickle.load(arquivo)
+    except FileNotFoundError as fnfe:
+        print(fnfe)
+        print("Arquivo nao encontrado, nenhum eleitor carregado!")
+
+    opcao = 1
+    while opcao in (1,2,3):
+        try:
+            opcao = menu_eleitor()
+
+            if opcao == 1:
+                inserir_eleitor(eleitores)
+            elif opcao == 2:
+                atualizar_eleitor(eleitores)
+            elif opcao == 3:
+                print("Saindo!")
+                break
+        except Exception as e:
+            #traceback.print_exc()
+            print(e)
