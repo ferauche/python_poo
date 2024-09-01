b1ca686d4736cac32dfd4bdc9180cb9c4c2a289c
ferauche <thiago.ferauche@gmail.com>
2024-09-01 20:19:45 -0300

    criada classe Candidato

---
+++ eleicoes/eleicao.py
@@ -0,0 +1,46 @@
+import pickle
+from typing import List
+from common import *
+
+class Urna:
+    mesario : Pessoa
+    __secao : int
+    __zona : int
+    __eleitores_presentes : List[Pessoa]
+    __votos = {} #dicionario chave = numero do candidato, valor é a quantidade de votos
+
+    def __init__(self, mesario : Pessoa, secao : int, zona : int,
+                 candidatos : List[Candidato], eleitores : List[Eleitor]):
+        self.mesario = mesario
+        self.__secao = secao
+        self.__zona = zona
+        self.__candidatos = candidatos
+        self.__eleitores = []
+        for eleitor in eleitores:
+            if eleitor.zona == zona and eleitor.secao == secao:
+                self.__eleitores.append(eleitor)
+
+        for candidato in self.__candidatos:
+            self.__votos[candidato.get_numero()] = 0
+        self.__votos['BRANCO'] = 0
+        self.__votos['NULO'] = 0
+
+        with open(f'{self.zona}_{self.secao}.pkl', 'wb') as arquivo:
+            pickle.dump(self.__votos, arquivo)
+
+    def get_eleitor(self, titulo : int):
+        for eleitor in self.__eleitores:
+            if eleitor.get_titulo() == titulo:
+                return True
+        return False
+
+    def registrar_voto(self, eleitor : Eleitor, n_cand : int):
+        self.__eleitores_presentes.append(eleitor)
+        if n_cand in self.__votos:
+            self.__votos[n_cand] += 1
+        else:
+            self.__votos['NULO'] += 1
+
+
+
+
